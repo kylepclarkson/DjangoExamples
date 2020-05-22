cc4317e3a9262871bf9d2f6e3000b90232c3b007
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-05-22 13:43:01 -0700

    Update
    Finished part 3.

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -19,10 +19,11 @@ posts = [
 def home(request):
     # past data to render request.
     context = {
+        'title':'HomePage',
         'posts':posts
     }
     # Render home.html
     return render(request, 'blog/home.html', context=context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html',context={'title':'about'})
