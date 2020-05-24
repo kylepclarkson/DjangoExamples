b52ea29b7a57995faf44bb158f9fb036aa02fe53
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-05-24 11:49:22 -0700

    Started part 12.

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -26,6 +26,18 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    # Route for user to reset password.
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+         name='password_reset'),
+    # Route when password reset is successful.
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    # Route when password reset is confirmed. Need token
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
     path('', include('blog.urls'))
 ]
 
