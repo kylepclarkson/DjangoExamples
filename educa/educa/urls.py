cbb8c9509737799cc0be7b1fb2a6872cbc38c79a
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-05-27 21:21:03 -0700

    Progress
    At 'restricting access to class-based views'

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),  #view for instructor or student to log in.
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),   #view for instructor or student to log out.
     path('admin/', admin.site.urls),
 ]
