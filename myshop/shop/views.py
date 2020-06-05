0468733cc12de63fe6c9257d5a5965604dfec2e3
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-06-04 19:05:39 -0700

    Added product views
    Added views for getting list of products and details of specifc product.

---
+++ myshop/shop/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Category, Product
+
+
+# Create your views here.
+
+def product_list(request, category_slug=None):
+    # Get list of products, filter by category_slug.
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    if category_slug:
+        # Filter products by category.
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                  'categories': categories,
+                  'products': products})
+
+def product_detail(request, id, slug):
+    # Get single product using id.
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product})
+
