c9a691c3574e747745f1bf85268c9223f942fc71
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-06-06 13:04:15 -0700

    Added form and views to cart object.
    Added form to add product. Added views for adding, removing, and seeing cart contents.

---
+++ myshop/cart/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+# Users can specify quantity of items in this range.
+PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range (1,21)]
+
+class CartAddProductForm(forms.Form):
+
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,
+                                      coerce=int)
+
+    override = forms.BooleanField(required=False,
+                                  initial=False,
+                                  widget=forms.HiddenInput)
