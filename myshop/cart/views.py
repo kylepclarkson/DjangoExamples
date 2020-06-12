eb4d0e50ce8ff3b023a1145e6be5dabfdc8dacdb
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-06-12 12:51:58 -0700

    Added cart context processor.
    Maintains cart contents across all templates.

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -42,18 +42,3 @@ def cart_detail(request):
 
     return render(request, 'cart/detail.html', {'cart': cart})
 
-def product_detail(request, id, slug):
-    # Display product details.
-    product = get_object_or_404(Product,
-                                id=id,
-                                slug=slug,
-                                available=True)
-
-    cart_product_form = CartAddProductForm()
-
-    return render(request,
-                  'shop/product/detail.html',
-                  {
-                      'product': product,
-                      'cart_product_form': cart_product_form
-                  })
