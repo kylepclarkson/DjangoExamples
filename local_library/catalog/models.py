b55e649ba64c6e5905f4b4ce08d73fdf73b91f0d
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2020-12-08 11:25:57 -0800

    Local library
    Finished model creation.

--- local_library/catalog/models.py
+++ local_library/catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+import uuid # For unique book instances.
 
 # Create your models here.
 
@@ -28,4 +29,57 @@ class Book(models.Model):
         """ Returns the url to access a detail record of this book. """
         return reverse('book-detail', args=[str(self.id)])
 
-# TODO define remaining class models. 
+class BookInstance(models.Model):
+    """ A specific instance of a book. """
+    # A unique id for this book instance.
+    id =    models.UUIDField(primary_key=True,
+                             default=uuid.uuid4,
+                             help_text='Unique ID for this instance of a book.')
+
+    book =  models.ForeignKey('Book',
+                              on_delete=models.SET_NULL,
+                              null=True)
+    # Details of specific book release.
+    imprint = models.CharField(max_length=200)
+    # The date in which this book instance is due to be returned.
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAD_STATUS = (
+        ('m', 'Maintenance'), ('o', 'On loan'), ('a', 'Available'), ('r', 'Reserved')
+    )
+    # The status of this book instance.
+    status  = models.CharField(
+        max_length=1,
+        choices=LOAD_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability.'
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f'{self.id} ({self.book.title})'
+
+class Author(models.Model):
+    """ A model representing the author of a book. """
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """ Returns the url to access an author instance. """
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+
+
+
