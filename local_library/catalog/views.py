e504401b105071839c52e969c0c7c607f33299df
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2020-12-09 12:45:09 -0800

    Local library update.

--- local_library/catalog/views.py
+++ local_library/catalog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+def index(request):
+    """ The home page of the site. """
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = \
+        BookInstance.objects.filter(status__exact='a').count()
+
+    num_authors = Author.objects.all().count()
+
+    # save values in context dict.
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
