f23e209308659f6fd2da56e46291d453fb64192a
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-01-03 13:27:51 -0800

    Create plain views to access question data.

--- basic_poll/polls/urls.py
+++ basic_poll/polls/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.super_basic_index, name='index'),
-    path('123/', views.super_basic_index, name='index123')
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- basic_poll/polls/views.py
+++ basic_poll/polls/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 import datetime
+from .models import Question
 
 # Create your views here.
 
-def super_basic_index(request):
-    return HttpResponse("<h1>Hello</h1> You're at the polls index. <p>" + str(datetime.datetime.now()) +"</p>")
+def index(request):
+    """ Display 5 newest questions. """
+    questions   = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'questions': questions,
+    }
+    return render(request, 'polls/index.html', context=context)
+
+def detail(request, question_id):
+    """ Display details of question. Raise 404 error if question id dne. """
+
+    try:
+        question = Question.objects.get(pk=question_id)
+    except:
+        raise Http404("This question does not exist!")
+    return render(request, 'polls/detail.html', context={'question': question})
+
+
+def results(request, question_id):
+    return HttpResponse(f"This is the results of question {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"This is voting on question: {question_id}")
+
+
 
