03eadc21e1d7b803a83b4d5c3b9a6b523928845b
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-01-06 12:40:01 -0800

    mysite
    Created home page.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls', 'home'), name='home'),
     path('blog/', include('blog.urls')),
 ]
