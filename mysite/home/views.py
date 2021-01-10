9075aaeff0c757bc9cd9a01e895d2ceb23da511d
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-01-09 20:13:39 -0800

    Modifying css files.

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
     return render(request, 'home/index.html')
+
+
+def test(request):
+    return render(request, 'home/test.html')
+
 
