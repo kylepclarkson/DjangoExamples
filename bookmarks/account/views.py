c98f9713734bc1578ae3112969b19be26bce5982
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-02-07 19:47:26 -0800

    Started bookmarks project.

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,126 +1,41 @@
+from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from django.contrib import messages
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
 
-# from .forms import LoginForm
-from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
-from .models import Profile
-
-@login_required
-def dashboard(request):
-    # Redirects user to dashboard.
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+from .forms import LoginForm
 
 
-# Allows for registering new user.
-def register(request):
-    # Register new user.
-    if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
+def user_login(request):
+    """ Handle user login. If method is GET, user will fill out form.
+    If method is POST, user has made an attempt to login."""
 
-        if user_form.is_valid():
-            # Create user, set password, then save.
-            new_user = user_form.save(commit=False)
-            new_user.set_password(
-                user_form.cleaned_data['password2']     # set_password hashes password.
-            )
-            new_user.save()
+    if request.method == 'GET':
+        form = LoginForm()
 
-            # Create profile for user
-            Profile.objects.create(user=new_user)
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
 
-            return render(request,
-                          'account/register_done.html',
-                          {'new_user': new_user})
-    else:
-        user_form = UserRegistrationForm()
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
 
-    return render(request,
-                  'account/register.html',
-                  {'user_form': user_form})
+            if user is not None:
+                # password matches username.
+                if user.is_active:
+                    # User is active. Log them in.
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
 
-# Allows for editing of profile.
-@login_required
-def edit(request):
+                else:
+                    # User is not active.
+                    return HttpResponse('Disabled account')
 
-    if request.method == 'POST':
-
-        user_form = UserEditForm(instance=request.user,
-                                 data=request.POST)
-        profile_form = ProfileEditForm(instance=request.user.profile,
-                                       data=request.POST,
-                                       files=request.FILES)
-
-        if user_form.is_valid() and profile_form.is_valid():
-            # Update User and Profile models.
-            user_form.save()
-            profile_form.save()
-
-            messages.success(request, 'Profile updated successfully.')
-
-        else:
-            messages.error(request, 'Error updating your profile. ')
-    else:
-        user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance=request.user.profile)
+            else:
+                # password does not match username.
+                return HttpResponse('Invalid Login')
 
     return render(request,
-                  'account/edit.html',
-                  {'user_form': user_form,
-                   'profile_form': profile_form})
-
-@login_required
-def user_list(request):
-    # Get all active users
-    users = User.objects.filter(is_active=True)
-
-    return render(request,
-                  'account/user/list.html',
-                  {'section': 'people',
-                   'users': users})
-
-@login_required
-def user_detail(request, username):
-    # Get active user with specified username.
-    user = get_object_or_404(User,
-                             username=username,
-                             is_active=True)
-
-    return render(request,
-                  'account/user/detail.html',
-                  {'section': 'people',
-                   'user': user})
-
-
-# Attempt to login in user.
-# Custom login view. Replaced by using Django's login
-# def user_login(request):
-#     # request method will be 'POST' or 'GET'
-#     if request.method == 'POST':
-#         form = LoginForm(request.POST)
-#
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#
-#             user = authenticate(request,
-#                                 username=cd['username'],
-#                                 password=cd['password'])
-#             if user is not None:
-#                 # user exists
-#                 if user.is_active:
-#                     login(request, user)
-#                     return HttpResponse('Authenticated successfully')
-#                 else:
-#                     # user's account is disabled.
-#                     return HttpResponse('Disabled account')
-#
-#             else:
-#                 return HttpResponse('Invalid login')
-#     else:
-#         # Set form for user to login
-#         form = LoginForm()
-#
-#     return render(request, 'account/login.html', {'form': form})
+                  'account/login.html',
+                  {'form': form})
--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -1,7 +1,7 @@
 """bookmarks URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,16 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
-    path('images/', include('images.urls', namespace='images')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
