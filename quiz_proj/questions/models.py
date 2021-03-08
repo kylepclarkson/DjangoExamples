3eaad0d244f04d3e3e1e9a54b4d3c911963eae8a
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-03-08 11:07:04 -0800

    Quiz App
     Added models.

--- quiz_proj/questions/models.py
+++ quiz_proj/questions/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from quizes.models import Quiz
+
 
 # Create your models here.
+
+class Question(models.Model):
+
+    text = models.CharField(max_length=200)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.text}"
+
+    def get_answers(self):
+        """ Returns all answers to this question """
+        return self.answer_set.all()
+
+
+class Answer(models.Model):
+    """ An answer to a question. """
+    text = models.CharField(max_length=200)
+    correct = models.BooleanField(default=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Question: {self.question.text}. Answer: {self.text}. Correct: {self.correct}"
+
--- quiz_proj/quizes/models.py
+++ quiz_proj/quizes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+DIFF_CHOICES = (
+    ('Easy', 'easy'),
+    ('Medium', 'medium'),
+    ('Hard', 'hard')
+)
+
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=120)
+    topic = models.CharField(max_length=120)
+    number_of_questions = models.IntegerField()
+    time = models.IntegerField(help_text='Duration of quiz in minutes')
+    required_score_to_pass = models.IntegerField(help_text='Score in percent')
+    difficulty = models.CharField(max_length=6, choices=DIFF_CHOICES)
+
+    def __str__(self):
+        return f"{self.name}-{self.topic}"
+
+    def get_questions(self):
+        """ Get all questions in this quiz (using reverse relationship) """
+        return self.question_set.all()
