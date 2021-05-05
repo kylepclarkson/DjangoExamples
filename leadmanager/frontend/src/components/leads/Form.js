3a0279170e71c75f4b608f5f414babf36a068ab1
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-05-04 20:51:59 -0700

    Created React imports.

---
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Add Lead form</h1>
+            </div>
+        )
+    }
+}
+
+export default Form
--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'leads',
+    'frontend',
+    
     'rest_framework',
 ]
 
